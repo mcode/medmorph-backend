db398d89b00360a2147e0c2ca9793dfdf5f216e3
kghoreshi <keeyan.ghoreshi@uconn.edu>
2021-01-08 00:57:51 -0500

    accept subscriptions

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ require('dotenv').config();
 
 const express = require('express');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const serversRouter = require('./routes/servers');
+const subscriptionsRouter = require('./routes/subscriptions');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json({
+    type: ['application/json', 'application/fhir+json']
+  }))
 
 app.use('/', indexRouter);
 app.use('/servers/', serversRouter);
-
+app.use('/notif/', subscriptionsRouter);
 module.exports = app;
---
+++ src/routes/subscriptions.js
@@ -0,0 +1,61 @@
+const { StatusCodes } = require('http-status-codes');
+
+const express = require('express');
+const router = express.Router();
+
+// POST /
+// Recieve subscription notification
+router.post('/:id', (req, res) => {
+    const id = req.params.id;
+    res.sendStatus(StatusCodes.OK);
+    const subscription = getSubscription(id);
+    useSubscription(subscription);
+  });
+
+router.put('/:id/:resource/:resourceId', (req, res) => {
+    const id = req.params.id;
+    res.sendStatus(StatusCodes.OK);
+    const subscription = getSubscription(id);
+    useSubscription(subscription, req.body);
+    
+});
+function getSubscription(id) {
+    // dummy function for getting the subscription
+    // which can contain information to inform
+    // the app what to do when notified
+    return {
+        subscriptionResource: 
+          {
+            'resourceType': 'Subscription',
+            'criteria': 'Patient?_id=1',
+            'channel': {
+            'type': 'rest-hook',
+            'endpoint': 'http://example.com/subscription/1234',
+            'header': ['content-type: application/fhir+json']
+            }
+          }
+        action: 'name'
+      };
+}
+function useSubscription(subscription, resource = null) {
+    // trigger criteria
+    const subscriptionResource = subscription.subscriptionResource;
+    const criteria = subscriptionResource.criteria;
+    if (subscriptionResource.channel.type === 'rest-hook') {
+        const header = {};
+        subscriptionResource.channel.header.forEach((element) => {
+            const pair = element.split(':');
+            header[pair[0]] = pair[1];
+        });
+
+        if (resource) {
+            // TODO
+        }
+    }
+
+    // TODO: Make the client do something based on the action/resource/criteria
+    const action = subscription.action;
+
+}
+
+module.exports = router;
