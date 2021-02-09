5105a63c0acdb801b5b8d3fe03c87064c319e413
Dylan <dehall@mitre.org>
2021-02-09 12:21:10 -0500

    cleanup + comments

--- src/utils/reporting_workflow.js
+++ src/utils/reporting_workflow.js
@@ -1,27 +1,55 @@
 /*
- * Definition of Reporting Workflow Context:
- *
- *
- */
 
+Definition of Reporting Workflow Context:
+{
+  planDefinition: the PlanDefinition for the Knowledge Artifact
+
+  actionSequence: array of indices pointing to actions in planDefinition.action
+   - assumption is that actions may be performed in any order, not necessarily 0,1,2,3...
+   - action sequence should be derived based on action.relatedAction
+
+  currentActionSequenceStep: index of which step in the actionSequence is
+    currently being processed
+}
+
+Try to keep the wiki page up to date with this implementation!
+https://github.com/mcode/medmorph-backend/wiki/Reporting-Workflow
+*/
+
+const BASE_REPORTING_BUNDLE_PROFILE =
+  'http://hl7.org/fhir/us/medmorph/StructureDefinition/us-ph-reporting-bundle';
+
+/**
+ * IG_REGISTRY maps the profile for a reporting bundle under an IG
+ * to the actions defined for action codes within that IG.
+ * Each content IG should have one JS file defined, which, when require()d,
+ * will register itself into this IG.
+ * Note that the base actions should be extend-able, so that content IGs
+ * do not have to re-implement codes unnecessarily.
+ */
 const IG_REGISTRY = {
-  'http://hl7.org/fhir/us/medmorph/StructureDefinition/us-ph-reporting-bundle': {} // base action definitions
+  [BASE_REPORTING_BUNDLE_PROFILE]: {} // base action definitions, see MEDMORPH-35
 };
 
 function initializeContext(planDefinition) {
-  // TODO
+  // TODO -- see MEDMORPH-36
   return { planDefinition };
 }
 
 function getFunction(ig, code) {
-  // function here in case this needs to be more complex
+  // use a function here in case this needs to be more complex
   return IG_REGISTRY[ig][code];
 }
 
+/**
+ * Find the target profile to be used when generating a bundle
+ * in support of the given PlanDefinition.
+ * The profile should be on the 'output' field of the 'create-report' action.
+ */
 function findProfile(planDefinition) {
   // figure out the profile of the target bundle
 
-  // TODO search arrays rather than hardcode?
+  // TODO search arrays rather than hardcode 0,0
   const createReportAction = planDefinition.action?.find(
     a => a.code[0].coding[0].code === 'create-report'
   );
@@ -74,7 +102,7 @@ function executeWorkflow(context) {
     const action = planDef.action[currentActionId];
 
     // TODO - check arrays for
-    // system ===  "http://hl7.org/fhir/us/medmorph/CodeSystem/us-ph-plandefinition-actions"
+    // system === "http://hl7.org/fhir/us/medmorph/CodeSystem/us-ph-plandefinition-actions"
     const actionCode = action.code[0].coding[0].code;
 
     const execute = getFunction(ig, actionCode);
@@ -87,10 +115,6 @@ function executeWorkflow(context) {
     context.currentActionSequenceStep++;
   }
 }
-
-// function deepCopy(o) {
-//   return JSON.parse(JSON.stringify(o));
-// }
 
 module.exports = {
   determineActionSequence,
