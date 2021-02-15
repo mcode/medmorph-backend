70314c1c930356cec5de67c1a6a61a65687ada91
dubem7 <7842704+dubem7@users.noreply.github.com>
2021-02-15 14:25:48 -0500

    Use client.js from PR#13

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,82 +1,79 @@
 const jose = require('node-jose');
 const axios = require('axios');
 const { v4 } = require('uuid');
+const queryString = require('query-string');
 const clientId = 'medmorph_backend';
 const keys = require('../keys/privateKey.json');
 
-class Client {
-  /**
-   *
-   * @param {*} jwks This clients private key set used for signing requests
-   */
-  constructor(jwks) {
-    this.jwks = jwks;
-  }
+/**
+ * Generate and return access token for the specified server. If tokenEndpoint
+ * is provided it will use that, otherwise it will query the smart configuration.
+ *
+ * @param {string} url  the fhir base url for the server to connect to
+ * @returns access token.
+ */
+async function connectToServer(url) {
+  // Generate the client_assertion jwt
+  const tokenEndpoint = await getTokenEndpoint(url);
+  const jwt = await generateJWT(clientId, tokenEndpoint);
 
-  /**
-   * Generate and return access token for the specified server
-   * @param {string} url  the fhir base url for the server to connect to
-   */
-  async connectToServer(url) {
-    const response = await axios.get(`${url}/.well-known/smart-configuration`);
-    const wellKnown = response.data.token_endpoint;
-    const jwt = await this.generateJWT(clientId, wellKnown, keys.kid);
+  const props = {
+    scope: 'system/*.read',
+    grant_type: 'client_credentials',
+    client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+    client_assertion: jwt
+  };
 
-    return jwt;
-  }
-
-  /**
-   * Get the local keystore that contains the JWKs for this client
-   */
-  async getKeystore() {
-    if (this.keystore) {
-      return this.keystore;
+  const headers = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept-Encoding': 'gzip, deflate, br',
+      Connection: 'keep-alive',
+      Accept: '*/*'
     }
-    if (this.jwks.keys) {
-      this.keystore = await jose.JWK.asKeyStore(this.jwks);
-    } else {
-      this.keystore = jose.JWK.createKeyStore();
-      this.keystore.add(this.jwks);
-    }
-    return this.keystore;
-  }
+  };
 
-  /**
-   * Get the key from the keystore for the kid provided.  If it is not there return
-   * the first key used for signing
-   * @param {string} kid - the kid of the key to lookup
-   */
-  async getKeyOrDefault(kid) {
-    const keystore = await this.getKeystore();
-    if (kid) {
-      return keystore.get(kid);
-    }
-    return keystore.all({ use: 'sig' });
-  }
-
-  /**
-   * Generate a signed JWT used for authenticating
-   * @param {*} client_id The identifier of the client on the remote server
-   * @param {*} aud The token url of the server the JWT is being created for
-   * @param {*} kid The identifier of the key in the JWKS to sign the JWT
-   */
-  async generateJWT(client_id, aud, kid = this.signingKeyId) {
-    // TODO: the spec allows for either RS384 or EC384 to be used
-    const options = { alg: 'RS384', compact: true };
-    const key = await this.getKeyOrDefault(kid);
-
-    const input = JSON.stringify({
-      sub: client_id,
-      iss: client_id,
-      aud: aud,
-      exp: Math.floor(Date.now() / 1000) + 300,
-      jti: v4()
-    });
-
-    return await jose.JWS.createSign(options, key)
-      .update(input)
-      .final();
-  }
+  // Get access token from auth server
+  const data = await axios
+    .post(tokenEndpoint, queryString.stringify(props), headers)
+    .then(response => response.data)
+    .catch(err => console.error(err));
+  return data.access_token;
 }
 
-module.exports = { Client: Client };
+/**
+ * Get the token_endpoint from the .well-known/smart-configuration
+ *
+ * @param {string} url - the fhir base url
+ * @returns token_endpoint
+ */
+async function getTokenEndpoint(url) {
+  const response = await axios.get(`${url}/.well-known/smart-configuration`);
+  return response.data.token_endpoint;
+}
+
+/**
+ * Generate a signed JWT used for authenticating
+ * @param {string} client_id The identifier of the client on the remote server
+ * @param {string} aud The token url of the server the JWT is being created for
+ */
+async function generateJWT(client_id, aud) {
+  // TODO: the spec allows for either RS384 or EC384 to be used
+  const options = { alg: 'RS384', compact: true };
+  const keystore = await jose.JWK.asKeyStore(keys);
+  const key = keystore.get(keys.keys[0].kid);
+
+  const input = JSON.stringify({
+    sub: client_id,
+    iss: client_id,
+    aud: aud,
+    exp: Math.floor(Date.now() / 1000) + 300,
+    jti: v4()
+  });
+
+  return await jose.JWS.createSign(options, key)
+    .update(input)
+    .final();
+}
+
+module.exports = { connectToServer };
