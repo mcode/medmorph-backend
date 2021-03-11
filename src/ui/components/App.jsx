a3d58c58d17bcb46da85552dd683f97f2adebc93
Benjamin Langley <blangley@mitre.org>
2021-03-11 16:18:10 -0500

    Add UI components

--- src/ui/components/App.jsx
+++ src/ui/components/App.jsx
@@ -1,15 +1,13 @@
 import './app.css';
 import React from 'react';
-import Auth from './Auth/Auth.jsx';
-import { UserProvider } from './UserProvider.jsx';
-// const UserProvider = require('./UserProvider').UserProvider;
+import Auth from './Auth';
+import { UserProvider } from './UserProvider';
 
 function App() {
   return (
     <div className={'app'}>
       <UserProvider>
         <Auth />
-        Medmorph
       </UserProvider>
     </div>
   );
