10c928f2fb2909646de41581a304bb6334120c57
Benjamin Langley <blangley@mitre.org>
2021-03-12 14:09:25 -0500

    Add debug logs

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,15 +2,15 @@ const express = require('express');
 const { StatusCodes } = require('http-status-codes');
 const passport = require('passport');
 const router = express.Router();
-// const debug = require('../storage/logs').debug('medmorph-backend:auth');
+const debug = require('../storage/logs').debug('medmorph-backend:auth');
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
-  // debug(`${req.user.uid} logged in`);
+  debug(`${req.user.uid} logged in`);
   res.send('Logged in!');
 });
 
 router.post('/logout', (req, res) => {
-  // debug(`${req.user.uid} logged out`);
+  debug(`${req.user.uid} logged out`);
   req.logOut();
   res.send('Logged out!');
 });
