0d37ae67eca230afef3fdfe17c86716ffa0387b2
kghoreshi <keeyan.ghoreshi@uconn.edu>
2021-05-07 13:01:37 -0400

    reorganize variables

--- src/storage/config.js
+++ src/storage/config.js
@@ -1,21 +1,22 @@
 const db = require('../storage/DataAccess');
 const { CONFIG } = require('./collections');
+const { configVars } = require('./configVars');
 
 /**
  * Fields in Config:
  *
  * Server:
- *  @param {string} ADMIN_TOKEN - the id of the server
- *  @param {string} AUTH - human readable name
- *  @param {string} AUTH_CERTS_URL: - FHIR base url
- *  @param {string} AUTH_TOKEN_URL - 'KA' | 'EHR' | 'PHA' - type of server the endpoint is
- *  @param {string} DATA_TRUST_SERVICE - the client ID assigned to this app during registration
- *  @param {string} REALM - the last access token received from authorization
+ *  @param {string} ADMIN_TOKEN - bearer token that grants access
+ *  @param {string} AUTH - auth base url
+ *  @param {string} AUTH_CERTS_URL: - auth url for certs retrieval
+ *  @param {string} AUTH_TOKEN_URL - auth url for token retrieval
+ *  @param {string} DATA_TRUST_SERVICE - the data trust service
+ *  @param {string} REALM - specific realm for the auth server
  *
  */
 
 function getAdminToken() {
-  const selection = db.select(CONFIG, c => c.key === 'ADMIN_TOKEN')[0];
+  const selection = db.select(CONFIG, c => c.key === configVars.ADMIN_TOKEN)[0];
   if (selection) {
     return selection.value;
   }
@@ -23,42 +24,42 @@ function getAdminToken() {
 
 function setAdminToken(value) {
   const entry = {
-    key: 'ADMIN_TOKEN',
+    key: configVars.ADMIN_TOKEN,
     value: value
   };
   db.upsert(CONFIG, entry, c => c.key === entry.key);
 }
 
 function getAuth() {
-  const selection = db.select(CONFIG, c => c.key === 'AUTH')[0];
+  const selection = db.select(CONFIG, c => c.key === configVars.AUTH)[0];
   if (selection) {
     return selection.value;
   }
 }
 
 function getAuthCertsUrl() {
-  const selection = db.select(CONFIG, c => c.key === 'AUTH_CERTS_URL')[0];
+  const selection = db.select(CONFIG, c => c.key === configVars.AUTH_CERTS_URL)[0];
   if (selection) {
     return selection.value;
   }
 }
 
 function getAuthTokenUrl() {
-  const selection = db.select(CONFIG, c => c.key === 'AUTH_TOKEN_URL')[0];
+  const selection = db.select(CONFIG, c => c.key === configVars.AUTH_TOKEN_URL)[0];
   if (selection) {
     return selection.value;
   }
 }
 
 function getDataTrustService() {
-  const selection = db.select(CONFIG, c => c.key === 'DATA_TRUST_SERVICE')[0];
+  const selection = db.select(CONFIG, c => c.key === configVars.DATA_TRUST_SERVICE)[0];
   if (selection) {
     return selection.value;
   }
 }
 
 function getRealm() {
-  const selection = db.select(CONFIG, c => c.key === 'REALM')[0];
+  const selection = db.select(CONFIG, c => c.key === configVars.REALM)[0];
   if (selection) {
     return selection.value;
   }
