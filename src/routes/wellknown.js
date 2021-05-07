5c6e8fbe56885300a6642d6183fe0859c1a92c8d
kghoreshi <keeyan.ghoreshi@uconn.edu>
2021-05-07 01:36:23 -0400

    move env to db config

--- src/routes/wellknown.js
+++ src/routes/wellknown.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const config = require('../storage/config');
 router.get('/smart-configuration', (req, res) => {
   const smartConfiguration = {
-    token_endpoint: process.env.AUTH_TOKEN_URL,
+    token_endpoint: config.getAuthTokenUrl(),
     response_types_supported: ['token'],
     scopes_supported: ['offline_access', 'system/*.*', 'system/*.read', 'system/*.write']
   };
