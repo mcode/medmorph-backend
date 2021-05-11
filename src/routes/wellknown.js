bf6c03f6451aacb46f1adef328d1705b070c6622
kghoreshi <keeyan.ghoreshi@uconn.edu>
2021-05-11 15:16:16 -0400

    certs/token url back to env

--- src/routes/wellknown.js
+++ src/routes/wellknown.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const configUtil = require('../storage/configUtil');
 router.get('/smart-configuration', (req, res) => {
   const smartConfiguration = {
-    token_endpoint: configUtil.getAuthTokenUrl(),
+    token_endpoint: process.env.AUTH_TOKEN_URL,
     response_types_supported: ['token'],
     scopes_supported: ['offline_access', 'system/*.*', 'system/*.read', 'system/*.write']
   };
