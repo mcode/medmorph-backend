0d9b6beb75cbeb38b58de529c43c765e019cec70
kghoreshi <keeyan.ghoreshi@uconn.edu>
2021-05-25 15:10:21 -0400

    empty trigger

--- src/utils/fhir.js
+++ src/utils/fhir.js
@@ -198,10 +198,10 @@ function postSubscriptionsToEHR(subscriptions) {
     if (ehrServer) {
       const subscriptionId = subscription.id;
       const fullUrl = `${ehrServer.endpoint}/Subscription/${subscriptionId}`;
-
+      const timestamp = Date.now();
       // Store subscriptions in database
       debug(`Saved Subscription/${subscriptionId}`);
-      db.upsert('subscriptions', { fullUrl, ...subscription }, s => compareUrl(s.fullUrl, fullUrl));
+      db.upsert('subscriptions', { timestamp, fullUrl, ...subscription }, s => compareUrl(s.fullUrl, fullUrl));
 
       // Create/Update Subscriptions on EHR server
       const ehrToken = await getAccessToken(ehrServer.endpoint);
