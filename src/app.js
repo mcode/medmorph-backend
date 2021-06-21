1325ec3bc7d1678c0436fab3b1574712415dfaf7
Benjamin Langley <blangley@mitre.org>
2021-06-21 10:59:21 -0400

    Test

--- src/app.js
+++ src/app.js
@@ -118,6 +118,6 @@ runWhenDBReady(refreshAllKnowledgeArtifacts);
 runWhenDBReady(subscribeToKnowledgeArtifacts);
 
 console.log('here!');
-console.log('test 4');
+console.log('test 5');
 
 module.exports = app;
